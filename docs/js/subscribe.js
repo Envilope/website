5f7489ee8921bb90cd0282e93f0b5b2d19c276d8
Yifan Wang <brettwy861@gmail.com>
2018-04-25 03:28:45 -0400

    add two form function

--- docs/js/subscribe.js
+++ docs/js/subscribe.js
@@ -13,13 +13,27 @@ var messagesRef = firebase.database().ref('email_list');
 
 // listen to form submit
 document.getElementById('sg-widget').addEventListener('submit', submitForm);
-document.getElementById('sg-widget-1').addEventListener('submit', submitForm);
+document.getElementById('sg-widget-1').addEventListener('submit', submitForm1);
 
 //submit form
 function submitForm(e){
 	e.preventDefault();
 	//get values
 	var email = getInputVal('sg_email');
+	//save messages
+	saveMessage(email);
+	// show alert error
+	document.querySelector('.sendgrid-subscription-widget .error').style.display = 'block';
+	//hide alert after 3 seconds
+	setTimeout(function(){
+		document.querySelector('.sendgrid-subscription-widget .error').style.display = 'none';
+	},3000);
+}
+
+function submitForm1(f){
+	f.preventDefault();
+	//get values
+	var email = getInputVal('sg_email_1');
 	//save messages
 	saveMessage(email);
 	// show alert error
